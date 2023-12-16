cb65ea39bcf368efd495905bb5b7b187faad3bf7
Deepak kumar <deepakkumar13204@gmail.com>
2023-12-16 22:30:42 +0530

    add file

--- app.js
+++ app.js
@@ -10,35 +10,22 @@ const methodOverride = require('method-override');
 
 
 // mongodb connection
-mongoose.connect(process.env.DB_URL,
-    {useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000})
+mongoose.connect(process.env.DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
 .then(()=>console.log("DB connect "))
 .catch(()=>{console.log("Failed to connect")})
    
 
-
-app.set('views', path.join(__dirname, 'views'));
-
-// Set view engine as EJS
+// file views set
+const views = path.join(__dirname,'views');
+app.set("views",views);
+app.set('view engine','ejs');
+app.use(express.static('public'))
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Middleware to handle form data
+app.set('views', '/opt/render/project/src/views');
 app.use(express.urlencoded({ extended: true }));
-
-// Example route to handle a 404 error
-app.use((req, res) => {
-    res.status(404).render('404'); // Assuming you have a '404' view
-  });
-
-  // Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).render('error'); // Assuming you have an 'error' view
-  });
-
+app.use(express.static(__dirname + '/public'));
 // /middleware
 app.use(express.static('uploads'))
 app.use(methodOverride('_method'))
